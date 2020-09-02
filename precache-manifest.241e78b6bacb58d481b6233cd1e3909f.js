050138ec491c5f492058130dd5494b61ee922022
beamwanatvhaya <wanatchaya.co.61@ubu.ac.th>
2020-09-02 16:07:17 +0700

    Updates

---
+++ precache-manifest.241e78b6bacb58d481b6233cd1e3909f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370d22af263d5d94c00e35e1e67b97a5",
+    "url": "/commath-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a5d4137317a7985c16ba",
+    "url": "/commath-tic-tac-toe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/commath-tic-tac-toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commath-tic-tac-toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d4137317a7985c16ba",
+    "url": "/commath-tic-tac-toe/static/js/main.abe24a17.chunk.js"
+  },
+  {
+    "revision": "842e157b7abbf4976d4d",
+    "url": "/commath-tic-tac-toe/static/js/runtime-main.f282ad86.js"
+  }
+]);
